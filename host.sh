3d834662524d5473742bd80a83cb9474862a019a
Noname <maratreal93@icloud.com>
2018-05-12 22:59:25 +0300

    yuiyu
    yiuy

---
+++ host.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get upgrade
+#sudo apt-get install xfce4 xfce4-goodies tightvncserver
+
+
+sudo apt-get -y install openssh-server
+sudo apt-get -y install vsftpd
