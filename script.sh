a33a9562d9dce9f679561cecbbb71e63b9d5e6a7
Noname <maratreal93@icloud.com>
2018-05-12 23:58:58 +0300

    1

--- script.sh
+++ script.sh
@@ -15,6 +15,7 @@ sudo apt-get install dialog
  2 "open ssh-server" off
  3 "open ssh-client" off
  4 "vsftpd" off
+ 5 "open-vm-tools-desktop" off
  )
  choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
  clear
@@ -23,27 +24,28 @@ sudo apt-get install dialog
  case $choice in
  
 1)
- #Install xfce4
  echo "Installing xfce4"
  apt install xfce4 xfce4-goodies tightvncserver -y
  ;;
 
 2)
- #open ssh-server
  echo "Installing open ssh-server"
  apt install openssh-server -y
  ;;
  
 3)
- #open ssh-client
  echo "Installing open ssh-client"
  apt install openssh-client -y
  ;;
  
 4) 
- #Install vsftpd
  echo "Installing vsftpd"
  apt install vsftpd -y
+ ;;
+ 
+5) 
+ echo "Installing open-vm-tools-desktop"
+ apt install open-vm-tools-desktop -y
  ;;
 
  esac
